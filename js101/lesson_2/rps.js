f2c56aaf282879d1bb16a335007fe26c43c4759c
Allison Embrey <allilearns@gmail.com>
2021-10-31 15:40:37 -0500

    Worked through bonus features 1 and 2 for rock paper scissors.

--- js101/lesson_2/rps.js
+++ js101/lesson_2/rps.js
@@ -1,68 +1,102 @@
-// Build a Rock Paper Scissors game. 
-// Our version of the game lets the user play 
-// against the computer. 
+// Build a Rock Paper Scissors game.
+// Our version of the game lets the user play
+// against the computer.
 
 const readLine = require('../../node_modules/readline-sync');
 
 const ROCK = 'rock';
 const PAPER = 'paper';
 const SCISSORS = 'scissors';
-const VALID_CHOICES = [ROCK, PAPER, SCISSORS];
+const SPOCK = 'spock';
+const LIZARD = 'lizard';
+const PRINT_CHOICES = [ROCK, PAPER, SCISSORS, SPOCK, LIZARD];
+
+const VALID_CHOICES = ['r', 'p', 'sc', 'sp', 'l'].concat(PRINT_CHOICES);
 const VALID_OUTCOMES = [`draw`, `player`, `computer`];
 
-playGame();
+const WINNING_COMBOS = {
+  [ROCK]:     [SCISSORS, LIZARD],
+  [PAPER]:    [ROCK, SPOCK],
+  [SCISSORS]: [PAPER, LIZARD],
+  [SPOCK]:    [SCISSORS, ROCK],
+  [LIZARD]:   [PAPER, SPOCK],
+};
 
-function playGame() {
-  const playerChoice = getPlayerChoice(`Choose one: ${VALID_CHOICES.join(', ')}.`);
+const CODE_MAP = {
+  [ROCK]: ['r'],
+  [PAPER]: ['p'],
+  [SCISSORS]: ['sc', 'c'],
+  [SPOCK]: ['sp', 'p'],
+  [LIZARD]: ['l'],
+};
+
+
+welcomeUser();
+
+do {
+  const playerChoice = getPlayerChoice(`Choose one: ${PRINT_CHOICES.join(', ')}.`);
   const computerChoice = getComputerChoice();
 
   reportChoices(playerChoice, computerChoice);
 
   const winner = determineWinner(playerChoice, computerChoice);
   console.log(formatWinner(winner));
-  
+
+} while (askUser(`Play again? (y/n)`));
+
+farewellUser();
+
+
+function welcomeUser() {
+  console.log(`Welcome to Rock Paper Scissors Spock Lizard!\n`);
+}
+
+function farewellUser() {
+  console.log(`Bye! Play again sometime!\n`);
 }
 
 function getPlayerChoice(message) {
   let choice = readLine.question(`${drawPrompt()} ${message}\n`);
-  
+
   while (!validChoice(choice)) {
-    choice = readLine.question(`${drawPrompt()} Sorry, we need one of '${VALID_CHOICES.join(', ')}.'\n`);
+    choice = readLine.question(`${drawPrompt()} Sorry, we need one of '${PRINT_CHOICES.join(', ')}.'\n`);
   }
-  
-  return choice.toLowerCase();
+
+  return formatChoice(choice);
 }
 
 function validChoice(choice) {
-  choice = choice.toLowerCase();  
+  choice = choice.toLowerCase();
   return VALID_CHOICES.includes(choice);
 }
 
+function formatChoice(choice) {
+  for (let property in CODE_MAP) {
+    if (CODE_MAP[property].includes(choice)) {
+      return property;
+    }
+  }
+
+  return choice;
+}
+
 function getComputerChoice() {
-  const randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  return VALID_CHOICES[randomIndex];
+  const randomIndex = Math.floor(Math.random() * PRINT_CHOICES.length);
+  return PRINT_CHOICES[randomIndex];
 }
 
 function reportChoices(player, computer) {
-  console.log(`${drawPrompt()} You chose ${player} and the computer chose ${computer}`);
+  console.log(`${drawPrompt()} You chose ${player} and the computer chose ${computer}.\n`);
 }
 
 function determineWinner(playerChoice, computerChoice) {
   const tie = playerChoice === computerChoice;
 
-  const playerBeatsScissors = playerChoice === ROCK && computerChoice === SCISSORS;
-  const playerBeatsPaper = playerChoice === SCISSORS && computerChoice === PAPER;
-  const playerBeatsRock = playerChoice === PAPER && computerChoice === ROCK;
-
-  const computerBeatsScissors = computerChoice === ROCK && playerChoice === SCISSORS;
-  const computerBeatsPaper = computerChoice === SCISSORS && playerChoice === PAPER;
-  const computerBeatsRock = computerChoice === PAPER && playerChoice === ROCK;
-
   if (tie) {
     return VALID_OUTCOMES[0];
-  } else if (playerBeatsScissors || playerBeatsPaper || playerBeatsRock) {
+  } else if (playerWins(playerChoice, computerChoice)) {
     return VALID_OUTCOMES[1];
-  } else if (computerBeatsScissors || computerBeatsPaper || computerBeatsRock) {
+  } else if (playerWins(computerChoice, playerChoice)) {
     return VALID_OUTCOMES[2];
   } else {
     return `Invalid game combination.`
@@ -72,6 +106,10 @@ function determineWinner(playerChoice, computerChoice) {
   }
 }
 
+function playerWins(choice1, choice2) {
+  return WINNING_COMBOS[choice1].includes(choice2);
+}
+
 function formatWinner(winner) {
   switch (winner) {
     case VALID_OUTCOMES[0]: return `It's a tie!`;
@@ -79,6 +117,22 @@ function formatWinner(winner) {
     case VALID_OUTCOMES[2]: return `Computer wins!`;
     default: return `${winner}`;
   }
+}
+
+
+function askUser(message) {
+  let answer = readLine.question(`${drawPrompt()} ${message}\n`).toLowerCase();
+
+  while (!validResponse(answer)) {
+    answer = readLine.question(`${drawPrompt()} ${message}\n`).toLowerCase();
+  }
+
+  return answer[0] !== 'n';
+}
+
+function validResponse(answer) {
+  const validAnswers = ['yes', 'y', 'yea', 'yeah', 'yep', 'no', 'n', 'nah', 'nope'];
+  return validAnswers.includes(answer);
 }
 
 function drawPrompt() {
