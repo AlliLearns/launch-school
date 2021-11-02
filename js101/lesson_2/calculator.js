aab9c4ee1d3c2e9aa7a8e475ace0a7e78596e899
Allison Embrey <allilearns@gmail.com>
2021-11-02 13:57:32 -0500

    Updated calculator.js according to feedback from code review.

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -1,8 +1,4 @@
-// Ask the user for the first number.
-// Ask the user for the second number.
-// Ask the user for an operation to perform.
-// Perform the operation on the two numbers.
-// Print the result to the terminal.
+// TODO: Move valid answers to the JSON file. 
 
 const readLine = require('../../node_modules/readline-sync');
 const MESSAGES = require('./calculator_messages.json');
@@ -14,24 +10,28 @@ const VALID_LANGUAGE_CODES = ['en', 'es', 'english', 'spanish', 'espaniol'];
 const langChoice = getLanguageChoice();
 greetUser();
 
+let keepGoing = false;
+
 do {
   console.log();
   const [number1, number2, operator] = getUserInput();
   const result = performOperation(number1, number2, operator);
   printResult(result);
 
-} while (askUser(MESSAGES[langChoice].askAnotherCalculation));
+  keepGoing = shouldGoAgain(MESSAGES[langChoice].askAnotherCalculation);
+
+  console.clear();
+
+} while (keepGoing);
 
 farewellUser();
 
 
 function getLanguageChoice() {
-  let lang = readLine.question(`${drawPrompt()} ${MESSAGES.promptLanguage}: `);
+  let lang = prompt(MESSAGES.promptLanguage);
 
   while (invalidLanguage(lang)) {
-    lang = readLine.question(`${drawPrompt()}`
-                              + ` Sorry, we don't recognize that language.\n`
-                              + `${drawPrompt()} ${MESSAGES.promptLanguage}: `);
+    lang = prompt(`${MESSAGES.languageNotKnown}.\n   ${MESSAGES.promptLanguage}`);
   }
 
   return lang.toLowerCase();
@@ -43,11 +43,12 @@ function invalidLanguage(code) {
 }
 
 function greetUser() {
+  console.clear();
   console.log(`\n${MESSAGES[langChoice].welcome}`);
 }
 
 function farewellUser() {
-  console.log(MESSAGES[langChoice].farewell);
+  console.log(`\n${MESSAGES[langChoice].farewell}\n`);
 }
 
 
@@ -60,10 +61,10 @@ function getUserInput() {
 }
 
 function promptForNumber(message) {
-  let num = readLine.question(`${drawPrompt()} ${message}: `);
+  let num = prompt(message);
 
   while (invalidNumber(num)) {
-    num = readLine.question(`${drawPrompt()} ${MESSAGES[langChoice].askNumAgain}: `);
+    num = prompt(MESSAGES[langChoice].askNumAgain);
   }
 
   return Number(num);
@@ -74,10 +75,10 @@ function invalidNumber(num) {
 }
 
 function promptForOperation(message) {
-  let operation = readLine.question(`${drawPrompt()} ${message}: `);
+  let operation = prompt(message);
 
   while (!VALID_OPERATORS.includes(operation)) {
-    operation = readLine.question(`${drawPrompt()} ${MESSAGES[langChoice].askOperatorAgain}: `);
+    operation = prompt(MESSAGES[langChoice].askOperatorAgain);
   }
 
   return Number(translateOperator(operation));
@@ -113,30 +114,32 @@ function handleDivideByZero(num1, num2) {
 
 function printResult(result) {
   if (typeof result === 'number') {
-    console.log(`\nThe result is: ${result}.\n`);
+    if (Number.isInteger(result)) {
+      console.log(`\n${MESSAGES[langChoice].theResultIs}: ${result}.\n`);
+    } else {
+      console.log(`\n${MESSAGES[langChoice].theResultIs}: ${result.toFixed(5)}.\n`);
+    }
   } else {
-    console.log(`\nSomething went wrong: ${result}.\n`);
+    console.log(`\n${MESSAGES[langChoice].somethingWentWrong}: ${result}.\n`);
   }
 }
 
 
-function askUser(message) {
-  let answer = readLine.question(`${drawPrompt()} ${message}: `).toLowerCase();
+function shouldGoAgain(message) {
+  let answer = prompt(message).toLowerCase();
 
   while (!validResponse(answer)) {
-    answer = readLine.question(`${drawPrompt()} ${MESSAGES[langChoice].askKeepGoingAgain}: `).toLowerCase();
+    answer = prompt(MESSAGES[langChoice].askKeepGoingAgain).toLowerCase();
   }
 
   return answer[0] !== 'n';
 }
 
 function validResponse(answer) {
-  const englishAnswers = ['yes', 'y', 'yea', 'yeah', 'yep', 'no', 'n', 'nah', 'nope'];
-  const spanishAnswers = ['si', 's', 'no', 'n'];
-  return englishAnswers.includes(answer) || spanishAnswers.includes(answer);
+  return MESSAGES[langChoice].yesNoResponses.includes(answer);
 }
 
-function drawPrompt() {
-  return ">>";
+function prompt(message) {
+  return readLine.question(`>> ${message}: `);
 }
 
