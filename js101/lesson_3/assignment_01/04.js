5d4843bad460b9cdc1f8effb59596d3cb0d7f8db
Allison Embrey <allilearns@gmail.com>
2021-11-10 11:01:02 -0600

    Committing the 'deleted' assignment_01 changes.

--- js101/lesson_3/assignment_01/04.js
+++
@@ -1,9 +0,0 @@
-// Using the following string, create a new string 
-// that contains all lowercase letters except for 
-// the first character, which should be capitalized. 
-
-let munstersDescription = "the Munsters are CREEPY and Spooky.";
-
-// ---------------------------------
-
-console.log(munstersDescription.charAt(0).toUpperCase() + munstersDescription.slice(1).toLowerCase());
