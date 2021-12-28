3e4fad3400016e92bbf05832a9d38a03a173f971
Allison Embrey <allilearns@gmail.com>
2021-12-28 10:36:41 -0600

    Started the first two exercises of the 'advanced01' problem set.

---
+++ js101/smallProblems/advanced01/01.js
@@ -0,0 +1,55 @@
+// Madlibs Revised
+// Let's build another program using madlibs. We made a similar program 
+// in the easy exercises, but this time the requirements are a bit 
+// different. Build a madlibs program that takes a test 'template' as 
+// input, substitutes in a section of randomized nouns, verbs and 
+// adjectives and adverbs to that text, and then returns it. 
+// You can build your own lists of nouns, verbs, adjectives and 
+// adverbs directly into your program. Your program should read this 
+// text and, for each line, place random words of the appropriate 
+// types into the text and return the result. 
+
+// The challenge of this program isn't just about writing your solution, 
+// it's about choosing the structure of the text templates. Choose the right 
+// way to structure your templates and this problem becomes much easier. 
+// Consequently, this exercises is a bit more open-ended since the input is 
+// also something you'll define for yourself. 
+
+// function madlibs(template) {
+//   // ...
+// }
+
+// // These examples use the following list of replacement texts:
+// // adjectives: quick lazy sleepy noisy hungry
+// // nouns: fox dog head leg tail cat
+// // verbs: jumps lifts bites licks pats
+// // adverbs: easily lazily noisily excitedly
+// // ------
+
+// madlibs(template1);
+// // The "sleepy" brown "cat" "noisily"
+// // "licks" the "sleepy" yellow
+// // "dog", who "lazily" "licks" his
+// // "tail" and looks around.
+
+// madlibs(template1);
+// // The "hungry" brown "cat" "lazily"
+// // "licks" the "noisy" yellow
+// // "dog", who "lazily" "licks" his
+// // "leg" and looks around.
+
+// madlibs(template2);      // The "fox" "bites" the "dog"'s "tail".
+
+// madlibs(template2);      // The "cat" "pats" the "cat"'s "head".
+
+/*
+  PROBLEM
+    input is a template
+    output is a string of words
+
+
+  ALGORITHM
+*/
+
+
+
