26153466ec34bb5377bf70f16b8f3b838736f475
Allison Embrey <allilearns@gmail.com>
2022-01-23 20:19:30 -0600

    Done with main rps program.

--- js120/lesson_01/oo_rps.js
+++ js120/lesson_01/oo_rps.js
@@ -30,15 +30,54 @@
 */
 
 
+const readLine = require('readline-sync');
 
-/* Make skeleton objects */
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+const VALID_MOVES = [ROCK, PAPER, SCISSORS];
+
+const WINNING_COMBO = {
+  [ROCK]: SCISSORS,
+  [SCISSORS]: PAPER,
+  [PAPER]: ROCK,
+}
+
 function createPlayer() {
-  const newPlayer = {
-    // state...
-    choose() {/*...*/},
+  const newPlayer = { move: null };
+  return newPlayer;
+}
+
+function createHuman() {
+  const newPlayer = createPlayer();
+
+  const newHuman = {
+    choose() {
+      let choice = promptUser(`Please choose 'rock', 'paper' or 'scissors'`).toLowerCase();
+
+      while (true) {
+        if (VALID_MOVES.includes(choice)) break;
+        console.log(`${drawPrompt()} Sorry, invalid choice.`);
+        choice = promptUser(`Please choose 'rock', 'paper' or 'scissors'`).toLowerCase();
+      }
+
+      this.move = choice;
+    },
   };
 
-  return newPlayer;
+  return Object.assign(newPlayer, newHuman);
+}
+
+function createComputer() {
+  const newPlayer = createPlayer();
+  const newComputer = {
+    choose() {
+      const randomIndex = Math.floor(Math.random() * VALID_MOVES.length);
+      this.move = VALID_MOVES[randomIndex];
+    },
+  };
+
+  return Object.assign(newPlayer, newComputer);
 }
 
 function createMove() {
@@ -57,21 +96,87 @@ const compare = function(move1, move2) {};
 
 
 
-/* Create an orchestration engine for the game */
-// The engine is where the procedural program flow should be. 
-
 const RPSGame = {
-  human:    createPlayer(),
-  computer: createPlayer(),
+  human:    createHuman(),
+  computer: createComputer(),
+  roundWinner: null,
 
+  displayWelcomeMessage() {
+    console.clear();
+    console.log(`-----------------------------------`);
+    console.log(` Welcome to Rock, Paper, Scissors!`);
+    console.log(`  Hit 'enter' to begin the game.`);
+    console.log(`-----------------------------------`);
+    readLine.prompt();
+    console.clear();
+  },
+
+  displayGoodbyeMessage() {
+    console.clear();
+    console.log(`--------------------------------`);
+    console.log(`     Thanks for playing!`);
+    console.log(`--------------------------------`);
+  },
+
+  displayMoves() {
+    const humanMove = this.human.move;
+    const computerMove = this.computer.move;
+
+    console.clear();
+    console.log(`--------------------------------`);
+    console.log(`You chose ${humanMove}.`);
+    console.log(`Computer chose ${computerMove}.`);
+    console.log(`--------------------------------`);
+    console.log(``);
+  },
+
+  determineWinner() {
+    const humanMove = this.human.move;
+    const computerMove = this.computer.move;
+
+    if (humanMove === computerMove) this.roundWinner = 'draw';
+    if (WINNING_COMBO[humanMove] === computerMove) this.roundWinner = 'player';
+    if (WINNING_COMBO[computerMove] === humanMove) this.roundWinner = 'computer';    
+  },
+
+  generateWinningMessage() {
+    switch(this.roundWinner) {
+      case 'draw':      return `It's a draw!`;
+      case 'player':    return `Congratulations, you win!`;
+      case 'computer':  return `Computer wins!`;
+      default:          return `Winner is indeterminate.`;
+    }
+  },
+
+  playAgain() {
+    console.log(``);
+    let again = promptUser(`Would you like to play again? (y/n)`);
+    return again.toLowerCase()[0] === `y`;
+  },
+ 
   play() {
-    displayWelcomeMessage();
-    this.human.choose();
-    this.computer.choose();
-    displayWinner();
-    displayGoodbyeMessage();
+    this.displayWelcomeMessage();
+
+    do {
+      console.clear();
+      this.human.choose();
+      this.computer.choose();
+      this.displayMoves();
+      this.determineWinner();
+      console.log(this.generateWinningMessage());
+    } while (this.playAgain())
+
+    this.displayGoodbyeMessage();
   },
 };
 
 
 RPSGame.play();
+
+function promptUser(question) {
+  return readLine.question(`${drawPrompt()} ${question}: `);
+}
+
+function drawPrompt() {
+  return `=>`
+}
