6fb37efb455300f63bc341f238dc22311595aa15
Allison Embrey <allilearns@gmail.com>
2022-03-03 20:58:49 -0600

    Worked on OO tic-tac-toe.

---
+++ js120/lesson_05_oolo/assignment02.js
@@ -0,0 +1,39 @@
+// My goal is to get through Launch School,
+// But the goal beyond that is what I need to switch my focus to: 
+// - Learn what's in front of me, and learn it well. 
+
+/*
+  Tic Tac Toe Overview
+*/
+
+/*  One approach to planning an object-oriented application: 
+    1. Write a textual description of the problem or exercise.
+    2. Extract the significant nouns and verbs from the description.
+    3. Organize and associate the verbs with the nouns.
+*/
+
+/*  1. Textual Description of Tic Tac Toe
+    TTT is a two-player board game.
+    The board is a 3 x 3 grid. 
+    Players take turns marking a square with a marker that IDs the player.
+    Traditionally, the player that uses `X` goes first, the player that uses `O` goes second.
+    The first player to mark three squares in a row wins the game.
+    A 'row' can be:
+      - Horizontal row
+      - Vertical column
+      - Either of the two diagonals.  
+    There is one human player and one computer player.
+    The human player always moves first. 
+*/
+
+/*  2. Identify Nouns and Verbs
+    Nouns: game, grid, players, marker, board
+    Verbs: play, mark, move, place
+*/
+
+/*  3. Associate Nouns and Verbs
+    Game: 
+    Board:  
+    Player: mark, human, computer
+*/
+
---
+++ js120/lesson_05_oolo/assignment04.js
@@ -0,0 +1,45 @@
+
+const boardPrototype = {};
+
+const playerPrototype = {};
+const humanPrototype = {};
+const computerPrototype= {};
+
+const TTTGame = {
+  board: Object.create(boardPrototype),
+
+  displayWelcomeMessage() { console.log(`Welcome`); },
+  displayGoodbyeMessage() { console.log(`Goodbye`); },
+  displayBoard() {},
+  displayGameResults() {},
+
+  firstPlayerMoves() {},
+  secondPlayerMoves() {},
+
+  playRound() {
+    console.log(`Played round of tic tac toe`);
+    do {
+      this.displayBoard();
+
+      this.firstPlayerMoves();
+      if (this.winnerFound()) break;
+  
+      this.secondPlayerMoves();
+      if (this.winnerFound()) break;
+    } while (!this.winnerFound());
+  },
+
+  winnerFound() { return true },
+
+
+  play() {
+    this.displayWelcomeMessage();
+
+    this.playRound();
+
+    this.displayGameResults();
+    this.displayGoodbyeMessage();
+  },
+};
+
+TTTGame.play();
